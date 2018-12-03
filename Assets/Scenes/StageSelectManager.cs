e353983038a2aaee016504782a08eb0c9495cdd7
Chiaki Mizuochi <m.chiaki.515@gmail.com>
2018-12-04 01:22:25 +0900

    シーン・ボタン追加，Ray検知失敗

---
+++ Assets/Scenes/StageSelectManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageSelectManager : MonoBehaviour
+{
+
+    GameObject clickedGameObject;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            clickedGameObject = null;
+
+            //指定の位置から発射されるRayを作成
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //Rayとオブジェクトの接触を調べる
+            RaycastHit2D hit2d = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
+
+            //接触してたらオブジェクトを小さくする
+            if (hit2d)
+            {
+                clickedGameObject = hit2d.transform.gameObject;
+                Debug.Log(clickedGameObject);
+            }
+        }
+    }
+
+}
